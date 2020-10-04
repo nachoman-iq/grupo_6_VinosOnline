c29acc8acbb6e04e62e88a2608bc16400538b192
nacho <ignacio.quiroga@gmail.com>
2020-10-04 00:06:31 -0300

    registro con express-valid

--- web/controllers/usersController.js
+++ web/controllers/usersController.js
@@ -1,7 +1,7 @@
-const bcrypt = require('bcrypt');
-//const { check, validationResult, body } = require('express-validator');
+const { check, validationResult, body } = require('express-validator'); //requerimos exp Validator
+const bcrypt = require('bcrypt'); 
 const fs = require('fs');
-const users = JSON.parse(fs.readFileSync(__dirname + "/../database/users.json"));
+const users = JSON.parse(fs.readFileSync(__dirname + "/../database/users.json")); //paras de users para uso global
 
 
 const controlador = {
@@ -11,7 +11,12 @@ const controlador = {
     },
 
     store: function (req, res) {
-        console.log("llego por post");   
+        console.log("llego por post"); 
+        let errors = validationResult(req);
+
+        if(!errors.isEmpty() ){
+            res.render('users/register',{errors:errors.errors})
+        }  
         let newUser = req.body;
         newUser.password = bcrypt.hashSync(newUser.password,10);
       
@@ -27,7 +32,7 @@ const controlador = {
        
         /******ver esto mas adelante para meter sesion y cookies*******/
         // req.session.usuariologueado = newUser;
-       // res.redirect("/users/profile/"+newUser.id);
+        //res.redirect("/products/"+newUser.nombre);
     }
     
 }
--- web/routes/users.js
+++ web/routes/users.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require("../controllers/usersController");
+const { check, validationResult, body } = require('express-validator'); //debo requerir validator en ruta y en contoller
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/register', usersController.register);
-router.post('/register', usersController.store);
+router.post('/register',[check("email").isEmail().withMessage("Email inválido"),
+check("nombre").isLength({ min: 2, max: 50 }).withMessage("Debes ingresar tu Nombre"),check("apellido").isLength({ min: 2, max: 50 }).withMessage("Completa tu Apellido")],usersController.store
+);
+
+
+//router.post('/register', usersController.store);
 
 module.exports = router;
